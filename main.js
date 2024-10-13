68140409879b60f9d47376f2621ddf5756477df7
gaboquerizo <gabrielbaquerizo2000@gmail.com>
2024-10-13 16:55:41 -0500

    Avances de la sección About_me

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('Archivo main.js conectado con éxito ✅');
+
+window.onload = function() {
+    document.body.style.zoom = "100%";
+}
+
+// function resetZoom() {
+//     document.body.style.zoom = "100%";
+// }
+// document.addEventListener("DOMContentLoaded", resetZoom);
